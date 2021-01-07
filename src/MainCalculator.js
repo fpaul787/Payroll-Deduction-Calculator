06e695f56f016bdaf006ccfc1fb725fd6a033439
Frantz Paul <fpaul787@gmail.com>
2021-01-07 16:29:10 -0500

    created backend math function for calculations

--- src/MainCalculator.js
+++ src/MainCalculator.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-
+import Grid from "@material-ui/core/Grid";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import calculate from "./calulate";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -81,27 +88,120 @@ const useStyles = makeStyles((theme) => ({
   },
 
   cssFocused: {},
+
+  button: {
+    background: "#d59f00",
+  },
+
+  copyButton: {
+    background: "#d59f00",
+    width: 100,
+  },
+
+  colorHighlight: {
+    color: "#d59f00",
+    "&.Mui-focused": {
+      color: "#d59f00",
+    },
+  },
+
+  textArea: {
+    height: 150,
+    width: 325,
+    resize: "none",
+    background: "#fff",
+    fontSize: 20,
+  },
+
+  radio: {
+    "&$checked": {
+      color: "#d59f00",
+    },
+  },
+  checked: {},
 }));
-
-const handleChange = (prop) => (event) => {
-  
-    const onlyNums = event.target.value.replace(/[^0-9]/g, '');
-    if (onlyNums.length < 10) {
-        // this.setState({ value: onlyNums });
-        console.log(onlyNums)
-    } else if (onlyNums.length === 10) {
-        const number = onlyNums.replace(
-            /(\d{3})(\d{3})(\d{4})/,
-            '($1) $2-$3'
-        );
-        // this.setState({ value: number });
-        console.log(number)
-    }
-}
-
 
 export default function MainCalculator() {
   const classes = useStyles();
+  const MINIMUM_PERCENTAGE = 0.1;
+  const [enteredTotal, setEnteredTotal] = useState(0);
+  const [periods, setPeriods] = useState(12);
+  const [downPayment, setDownPayment] = useState(0);
+
+  const [textAreaValue, setTextAreaValue] = useState(
+    `Please make some calculations`
+  );
+
+  const validateNumber = (total) => {
+    var totalString = total.toString();
+    let isnum = /^\d+(\.\d{1,2})?$/.test(totalString);
+    return isnum;
+  };
+
+  const handleEnteredTotal = (event) => {
+    setEnteredTotal(event.target.value);
+  };
+
+  const handlePeriod = (event) => {
+    setPeriods(event.target.value);
+  };
+
+  const handleDownPayment = (event) => {
+    setDownPayment(event.target.value);
+  };
+
+  const handleSubmitFromTotal = (event) => {
+    if (!validateNumber(enteredTotal)) {
+      alert("Total must be greater equal to 0");
+      return;
+    }
+
+    var tempDownpayment = enteredTotal * MINIMUM_PERCENTAGE;
+
+    var { calculatedDownpayment, paymentPerPeriod } = calculate(
+      enteredTotal,
+      tempDownpayment,
+      periods
+    );
+
+    setTextAreaValue(
+      `Total: $${enteredTotal}\nDown Payment: $${calculatedDownpayment}\nRemaining: $${
+        enteredTotal - calculatedDownpayment
+      }\nPayment Per ${periods} Periods: $${paymentPerPeriod}
+    `
+    );
+  };
+
+  const handleSubmitFromDownPayment = (event) => {
+    if (!validateNumber(enteredTotal)) {
+      alert("Total must be greater or equal to 0");
+      return 
+    }
+
+    if(!validateNumber(downPayment)){
+      alert("Down Payment must be greater equal to 0");
+      return; 
+    }
+
+    if(downPayment < enteredTotal * MINIMUM_PERCENTAGE){
+      alert("Down Payment must be greater or equal to 10% of total");
+      return; 
+    }
+
+    var { calculatedDownpayment, paymentPerPeriod } = calculate(
+      enteredTotal,
+      downPayment,
+      periods
+    );
+
+    setTextAreaValue(
+      `Total: $${enteredTotal}\nDown Payment: $${calculatedDownpayment}\nRemaining: $${
+        enteredTotal - calculatedDownpayment
+      }\nPayment Per ${periods} Periods: $${paymentPerPeriod}
+    `
+    );
+  };
+
   return (
     <main className={classes.layout}>
       <Paper elevation={1} className={classes.paper}>
@@ -113,35 +213,150 @@ export default function MainCalculator() {
         >
           New Payroll Deduction
         </Typography>
-        <form className={classes.form} noValidate autoComplete="off">
-          <TextField
-            id="total"
-            variant="outlined"
-            label="Total"
-            type="number"
-            className={classes.textField}
-            onChange={handleChange("total")}
-            InputProps={{
-              className: classes.input,
-              startAdornment: (
-                <InputAdornment position="start">$</InputAdornment>
-              ),
-              classes: {
-                root: classes.cssOutlinedInput,
-                focused: classes.cssFocused,
-                notchedOutline: classes.notchedOutline,
-              },
-            }}
-            InputLabelProps={{
-              style: { color: "#d59f00" },
-              classes: {
-                root: classes.labelRoot,
-                focused: classes.labelFocused,
-              },
-            }}
-          />
-          
-        </form>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={8}>
+            <TextField
+              id="total"
+              variant="outlined"
+              label="Total"
+              type="number"
+              className={classes.textField}
+              onChange={handleEnteredTotal}
+              InputProps={{
+                className: classes.input,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <span className={classes.colorHighlight}>$</span>
+                  </InputAdornment>
+                ),
+                classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+                },
+              }}
+              InputLabelProps={{
+                style: { color: "#d59f00" },
+                classes: {
+                  root: classes.labelRoot,
+                  focused: classes.labelFocused,
+                },
+              }}
+            />
+          </Grid>
+          <Grid style={{ marginLeft: "10px" }} item xs={12} sm={8}>
+            <FormControl component="fieldset" className={classes.form}>
+              <FormLabel className={classes.colorHighlight} component="legend">
+                Periods
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-label="period"
+                name="period"
+                value={periods.toString()}
+                onChange={handlePeriod}
+              >
+                <FormControlLabel
+                  value="12"
+                  control={
+                    <Radio
+                      disableRipple
+                      classes={{
+                        root: classes.radio,
+                        checked: classes.checked,
+                      }}
+                    />
+                  }
+                  label="12"
+                />
+                <FormControlLabel
+                  value="16"
+                  control={
+                    <Radio
+                      classes={{
+                        root: classes.radio,
+                        checked: classes.checked,
+                      }}
+                    />
+                  }
+                  label="16"
+                />
+                <FormControlLabel
+                  value="20"
+                  control={
+                    <Radio
+                      classes={{
+                        root: classes.radio,
+                        checked: classes.checked,
+                      }}
+                    />
+                  }
+                  label="20"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid style={{ marginLeft: "10px" }} item xs={12} sm={8}>
+            <Button
+              className={classes.button}
+              onClick={handleSubmitFromTotal}
+              variant="contained"
+            >
+              Calculate From Total
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <TextField
+              id="downPayment"
+              variant="outlined"
+              label="Down Payment"
+              type="number"
+              onChange={handleDownPayment}
+              className={classes.textField}
+              InputProps={{
+                className: classes.input,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <span className={classes.colorHighlight}>$</span>
+                  </InputAdornment>
+                ),
+                classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+                },
+              }}
+              InputLabelProps={{
+                style: { color: "#d59f00" },
+                classes: {
+                  root: classes.labelRoot,
+                  focused: classes.labelFocused,
+                },
+              }}
+            />
+          </Grid>
+          <Grid style={{ marginLeft: "10px" }} item xs={12} sm={8}>
+            <Button
+              className={classes.button}
+              onClick={handleSubmitFromDownPayment}
+              variant="contained"
+            >
+              Calculate From Down Payment
+            </Button>
+          </Grid>
+          <Grid style={{ marginLeft: "10px" }} item xs={12} sm={8}>
+            <textarea
+              className={classes.textArea}
+              disabled
+              value={textAreaValue}
+            />
+          </Grid>
+          <Grid style={{ marginLeft: "10px" }} item xs={12} sm={8}>
+            <Button className={classes.copyButton} variant="contained">
+              Copy
+            </Button>
+          </Grid>
+        </Grid>
       </Paper>
     </main>
   );
---
+++ src/calulate.js
@@ -0,0 +1,23 @@
+
+
+export default function calculate(totalAmt, downPayment, numberOfPayments) {
+  /** Initial Calculation */
+
+  var remainingAmt = totalAmt - downPayment;
+
+  var paymentPerPeriod = remainingAmt / numberOfPayments;
+
+  /** Clean Calculation with even number */
+
+  // round payment per period down to nearest dollar
+  paymentPerPeriod = Math.floor(paymentPerPeriod);
+
+  // multiply by number of payments to get remaining
+  remainingAmt = paymentPerPeriod * numberOfPayments;
+
+  // substract total minus remaining to get down payment
+  downPayment = Math.round((totalAmt - remainingAmt) * 100) / 100;
+
+  var calculatedDownpayment = downPayment;
+  return { calculatedDownpayment, paymentPerPeriod };
+}
