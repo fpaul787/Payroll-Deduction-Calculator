ee76600831e6f596493839621261b0f969c4ad8e
Frantz Paul <fpaul787@gmail.com>
2022-10-15 23:07:58 -0400

    Adding Analytics

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "./theme";
 import Navbar from "./Navbar";
 import NewPayroll from './NewPayroll';
+import ReactGA from 'react-ga';
+import {useEffect} from 'react';
+
+const TRACKING_ID = "UA-159676073-1"; // Tracking ID
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
